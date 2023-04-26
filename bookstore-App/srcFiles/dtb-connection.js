9da9972d91a5fb06b09fdb6197d610ebd62f9cfd
Anna Kašíková <106098004+anna9393@users.noreply.github.com>
2023-04-26 10:36:49 +0200

    Update dtb-connection.js

--- bookstore-App/srcFiles/dtb-connection.js
+++ bookstore-App/srcFiles/dtb-connection.js
@@ -1,11 +1,11 @@
 const mySQL = require('mysql2');
 
 const conn = mySQL.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
+    host: '',
+    port: '',
+    user: '',
     database: 'bookinfo',
-    password: 'password'
+    password: ''
 });
 
 module.exports = conn;
